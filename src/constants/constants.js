06681938d3d15c4382e7e3a8769313562e4692ff
Diego <disofu234@gmail.com>
2022-10-17 23:23:16 -0700

    Finally, the culprit, wrong UI root URL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -282,4 +282,4 @@ const DEV_IPADDRESSES = {
 
 export const ROOT_URL = `https://scheduleit-api-production.up.railway.app`
 
-export const UI_ROOT_URL = `https://fantastic-kulfi-0a09ff.netlify.app`
+export const UI_ROOT_URL = `https://scheduleanevent.netlify.app`
